25dc5dd9930782fb838681ba3306094d7962a8d9
propa <propa@DESKTOP-10ADTBO>
2016-06-02 23:31:11 +0200

    javadoc en metodo conectar

---
+++ src/LibreriaCD/Metodos.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LibreriaCD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author propa
+ */
+public class Metodos {
+
+    private Connection connection = null;
+    private ResultSet rs = null;
+
+    /**
+     * conecta con la base, con el driver y parámetros
+     *
+     * @param url recoge el nombre de la base
+     * @param user recoge el nombre de usuario
+     * @param pass recoge la contraseña del usuario
+     */
+    public void conectar(String url, String user, String pass) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/" + url, user, pass);
+            System.out.println("conectado");
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void insert(String tabla, String datos) {
+        PreparedStatement i;
+        try {
+            i = connection.prepareStatement("insert into " + tabla + " values(" + datos + ")");
+            i.execute();
+            System.out.println("insertado");
+        } catch (SQLException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void delete(String tabla, int primaryKey) {
+        PreparedStatement d;
+        try {
+            d = connection.prepareStatement("delete from " + tabla + " where id=" + primaryKey);
+            d.execute();
+            System.out.println("eliminado");
+        } catch (SQLException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void update(String tabla, String campo, String dato, int primaryKey) {
+        PreparedStatement u;
+        try {
+            u = connection.prepareStatement("update " + tabla + " set " + campo + "=" + dato + " where .id=" + primaryKey);
+            u.execute();
+            System.out.println("actualizado");
+        } catch (SQLException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public ResultSet select(String tabla, String campos) {
+        Statement s;
+        try {
+            s = connection.createStatement();
+            rs = s.executeQuery("select " + campos + " from " + tabla);
+        } catch (SQLException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+
+    public void apagar() {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
